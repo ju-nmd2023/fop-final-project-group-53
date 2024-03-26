ac3cc322d3c0a0a1e2721d6bf7b49ab16cce6e08
olne23pf <olne23pf@student.ju.se>
2024-03-26 11:42:37 +0100

    upd

--- game.js
+++ game.js
@@ -1,9 +1,16 @@
 function setup() {
+
     createCanvas(600,400);
     frameRate(30);
 }
 
 function scenery() {
-background-image: URL();
-}
+//sky
+let backgroundImage = document.getElementById("skyImage");
+skyImage.setAttribute("src", "/images/images/ian-keefe-0BVkO0t6oCE-unsplash.jpg");
+
+backgroundImage.drawImage(skyImage,0,0,100,100)
+//bridge
+
+pop()}
 
