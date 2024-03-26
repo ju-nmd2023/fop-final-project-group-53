c7cc87d1ead867bc6fe2b7eecd910cef6d88c7d1
olne23pf <olne23pf@student.ju.se>
2024-03-26 12:13:19 +0100

    test background

--- game.js
+++ game.js
@@ -1,16 +1,21 @@
 function setup() {
-
-    createCanvas(600,400);
+    createCanvas(400,400);
+    stroke();
     frameRate(30);
 }
+let x = 0;
+let y = 0;
+
 
 function scenery() {
+
 //sky
 let backgroundImage = document.getElementById("skyImage");
 skyImage.setAttribute("src", "/images/images/ian-keefe-0BVkO0t6oCE-unsplash.jpg");
 
-backgroundImage.drawImage(skyImage,0,0,100,100)
 //bridge
+fill(20,20,20);
+rect(0,100,400,50)
 
-pop()}
+}
 
