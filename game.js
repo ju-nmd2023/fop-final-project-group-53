efc8a1a7d9e18ea7926f35d72eaa462f2ab2e5c5
olne23pf <nellieolssonwennberg@gmail.com>
2024-04-04 14:29:18 +0200

    upd

--- game.js
+++ game.js
@@ -1,21 +1,12 @@
 function setup() {
-    createCanvas(400,400);
-    stroke();
-    frameRate(30);
+  createCanvas(400, 400);
+  frameRate(30);
 }
 let x = 0;
 let y = 0;
 
-
 function scenery() {
-
-//sky
-let backgroundImage = document.getElementById("skyImage");
-skyImage.setAttribute("src", "/images/images/ian-keefe-0BVkO0t6oCE-unsplash.jpg");
-
-//bridge
-fill(20,20,20);
-rect(0,100,400,50)
-
+  //bridge
+  fill(20, 20, 20);
+  rect(0, 100, 400, 50);
 }
-
