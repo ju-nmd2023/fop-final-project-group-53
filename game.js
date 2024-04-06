8587d8846f5b86f53f2e9257fe74dc803c820859
olne23pf <nellieolssonwennberg@gmail.com>
2024-04-06 12:14:12 +0200

    start p5
    background and start with sprite(character)

--- game.js
+++ game.js
@@ -1,12 +1,25 @@
+let mySprite; //Video game developers use the word "sprite" to refer to characters, items, or anything else that moves above a background.
+let img;
+let spriteStatic;
+
+function preload() {
+  img = loadImage("images/backgroundImg.jpg");
+  //spriteStatic = loadImage("images/flintStartPosition.png");
+}
 function setup() {
   createCanvas(400, 400);
-  frameRate(30);
-}
-let x = 0;
-let y = 0;
+  //frameRate(30);
+  mySprite = createSprite(200, 200, 20, 20);
+  mySprite.shapeColor = [0, 0, 200];
+  //friction = 0.03;
+  // mySprite.onMousePressed = function () {
+  // mySprite.rotationSpeed = 1;
+  //};
 
-function scenery() {
-  //bridge
-  fill(20, 20, 20);
-  rect(0, 100, 400, 50);
+  //mySprite.addImage(spriteStatic);
+  // mySprite.scale = 0.2;
+}
+function draw() {
+  background(img);
+  drawSprites();
 }
