bf4b643786db766c747e508ffff3b57dd75cb66c
olne23pf <nellieolssonwennberg@gmail.com>
2024-04-06 14:50:49 +0200

    upd

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-let mySprite; //Video game developers use the word "sprite" to refer to characters, items, or anything else that moves above a background.
-let img;
+let character;
 let spriteStatic;
 
 function preload() {
@@ -10,17 +9,18 @@ function setup() {
   const canvas = createCanvas(800, 600);
   canvas.parent("sketch-holder"); //move the canvas so it's inside our <div id="sketch-holder">
   frameRate(30);
-  //mySprite = createSprite(200, 200, 20, 20);
-  //mySprite.shapeColor = [0, 0, 200];
+  //character = new Sprite();
+  //character.diameter = 50;
   //friction = 0.03;
-  // mySprite.onMousePressed = function () {
-  // mySprite.rotationSpeed = 1;
+  // character.onMousePressed = function () {
+  // character.rotationSpeed = 1;
   //};
 
-  //mySprite.addImage(spriteStatic);
-  // mySprite.scale = 0.2;
+  //character.addImage(spriteStatic);
+  // character.scale = 0.2;
 }
+
 function draw() {
   background(img);
-  drawSprites();
+  //drawSprites();
 }
