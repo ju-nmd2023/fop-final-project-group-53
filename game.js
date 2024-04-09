44be212f5b514417a3e16ad314d788e591cff0e7
olne23pf <nellieolssonwennberg@gmail.com>
2024-04-09 11:36:43 +0200

    new images

--- game.js
+++ game.js
@@ -3,24 +3,12 @@ let spriteStatic;
 
 function preload() {
   img = loadImage("images/backgroundImg.jpg");
-  //spriteStatic = loadImage("images/flintStartPosition.png");
 }
 function setup() {
   const canvas = createCanvas(800, 600);
-  canvas.parent("sketch-holder"); //move the canvas so it's inside our <div id="sketch-holder">
   frameRate(30);
-  //character = new Sprite();
-  //character.diameter = 50;
-  //friction = 0.03;
-  // character.onMousePressed = function () {
-  // character.rotationSpeed = 1;
-  //};
-
-  //character.addImage(spriteStatic);
-  // character.scale = 0.2;
 }
 
 function draw() {
   background(img);
-  //drawSprites();
 }
