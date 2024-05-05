c267d7e06f15d52619778c304cdc4537439b690e
olne23pf <nellieolssonwennberg@gmail.com>
2024-05-05 14:37:51 +0200

    Update, to p5

--- p5Game.js
+++ p5Game.js
@@ -1,33 +1,113 @@
+let canvasWidth = 700;
+let canvasHeight = 500;
 let backgroundimg;
 let startimg;
 let img;
-let fontStartScreen;
+let myFont;
+let button;
+let button2;
+
+let gameHeight;
+let gameWidth;
+
+function preload() {
+  // myFont = loadFont("LilitaOne-Regular.ttf");
+  //for start screen
+  startimg = loadImage("images/startscreen_image.png");
+  //for game screen
+  backgroundimg = loadImage("images/backgroundImg.jpg");
+  img = loadImage("images/sprites2.png");
+}
 
 function setup() {
-  startimg = loadImage("images/startscreen_image.png");
-  backgroundimg = loadImage("images/backgroundImg.jpg");
-  img = loadImage("images/sprites.png");
-  fontStartScreen = loadFont("fonts/GillSansCondUltraBoNova.ttf");
-  createCanvas();
+  createCanvas(canvasWidth, canvasHeight);
+  startScreen();
+  textFont("Times New Roman");
+  player = new Player(canvasWidth, canvasHeight); //creates player
 }
 
 function startScreen() {
   background(startimg);
-  textSize(20);
+  textSize(65);
   textAlign(CENTER);
-  textFont(fontStartScreen);
-  text("Rainy with a chance of meatballs", width / 2, height / 2 - 50);
+  textStyle(BOLD);
+  text("Cloudy", width / 2, height / 2 - 140);
+  text("with a chance of", width / 2, height / 2 - 70);
+  text("meatballs", width / 2, height / 2);
+
   //Play Button
-  let button = createButton("PLAY");
-  button.position(width / 2, height / 2);
+  let button = createButton("Start Game");
+  button.position(canvasWidth / 4, height / 2 + 50);
+  button.size(120, 40);
   button.mousePressed(gameScreen);
+  let button2 = createButton("Controls");
+  button2.position(canvasWidth / 2 + canvasWidth / 7, height / 2 + 50);
+  button2.size(120, 40);
+  button2.mousePressed(controlScreen);
 }
 
 function gameScreen() {
+  clear();
   background(backgroundimg);
-  image(img, 0, 0);
+  // button.remove();
+  //button2.remove();
+}
+
+class Player {
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = canvasWidth;
+    this.gameHeight = canvasHeight;
+    this.imgWidth = 200;
+    this.imgHeight = 166;
+    this.x = 0;
+    this.y = this.gameHeight - this.imgHeight - 100;
+    this.frameX = 2;
+    this.frameY = 0;
+    this.maxFrame = 9;
+    this.fps = 20;
+    this.frameTimer = 5;
+    this.frameInterval = 1000 / this.fps;
+    this.speed = 0;
+    this.vy = 0;
+    this.weight = 1;
+  }
+
+  draw() {
+    //push();
+    image(
+      img,
+      this.x,
+      this.y,
+      this.imgWidth,
+      this.imgHeight,
+      0,
+      0,
+      this.imgWidth,
+      this.imgHeight
+    );
+    this.frameX++;
+    //pop();
+  }
+
+  update() {
+    //sprite animation, learnt from this tutorial https://www.youtube.com/watch?v=7JtLHJbm0kA&t=1675s
+    /* if (frameTimer > frameInterval) {
+      if (frameX >= maxFrame) frameX = 0;
+      else frameX++;
+      frameTimer = 0; //resets the frameTimer back to zero after making an animation of all 10 (0-9) frames
+    } else {
+      //frameTimer += deltaTime;
+    }*/
+    //controls
+  }
+}
+
+function controlScreen() {
+  clear();
 }
 
 function draw() {
   gameScreen();
+  player.draw();
+  player.update();
 }
