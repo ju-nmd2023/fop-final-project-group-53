2af83304e2db5560bdc7a6f47d64c66129b6ecce
olne23pf <nellieolssonwennberg@gmail.com>
2024-05-06 13:40:59 +0200

    upd

--- p5Game.js
+++ p5Game.js
@@ -21,6 +21,7 @@ function preload() {
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
+  frameRate(30);
   startScreen();
   textFont("Times New Roman");
   player = new Player(canvasWidth, canvasHeight); //creates player
@@ -54,16 +55,17 @@ function gameScreen() {
 }
 
 class Player {
+  //sprite code, learnt from this tutorial https://www.youtube.com/watch?v=7JtLHJbm0kA&t=1675s
   constructor(gameWidth, gameHeight) {
-    this.gameWidth = canvasWidth;
-    this.gameHeight = canvasHeight;
-    this.imgWidth = 200;
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.imgWidht = 200;
     this.imgHeight = 166;
     this.x = 0;
-    this.y = this.gameHeight - this.imgHeight - 100;
-    this.frameX = 2;
+    this.y = this.gameHeight - this.imgHeight;
+    this.frameX = 0;
     this.frameY = 0;
-    this.maxFrame = 9;
+    this.maxFrame = 10;
     this.fps = 20;
     this.frameTimer = 5;
     this.frameInterval = 1000 / this.fps;
@@ -73,32 +75,64 @@ class Player {
   }
 
   draw() {
-    //push();
+    //sprite image by https://p5js.org/reference/#/p5/image
     image(
       img,
-      this.x,
-      this.y,
-      this.imgWidth,
+      this.x + 250,
+      this.y - 95,
+      this.imgWidht,
       this.imgHeight,
-      0,
-      0,
-      this.imgWidth,
+      this.frameX * this.imgWidht,
+      this.frameY * this.imgHeight,
+      this.imgWidht,
       this.imgHeight
-    );
-    this.frameX++;
-    //pop();
+    ); //draws the player
+    //this.frameX = (this.frameX + 1) % this.maxFrame;
   }
 
   update() {
-    //sprite animation, learnt from this tutorial https://www.youtube.com/watch?v=7JtLHJbm0kA&t=1675s
-    /* if (frameTimer > frameInterval) {
-      if (frameX >= maxFrame) frameX = 0;
-      else frameX++;
-      frameTimer = 0; //resets the frameTimer back to zero after making an animation of all 10 (0-9) frames
-    } else {
-      //frameTimer += deltaTime;
-    }*/
+    //millis() returns the number of milliseconds since the sketch started.
+    if (millis() - this.frameTimer > this.frameInterval) {
+      this.frameX = (this.frameX + 1) % this.maxFrame;
+      this.frameTimer = millis(); // Reset the frame timer
+    }
+
     //controls
+    if (keyCode == RIGHT_ARROW) {
+      this.x += this.speed;
+      this.frameY = 0;
+    }
+    if (keyCode == LEFT_ARROW) {
+      this.x -= this.speed;
+      this.frameY = 1;
+    }
+    if (keyCode == UP_ARROW && this.onGround()) {
+      this.vy -= 14; //velocity on 20, meaning that when jumping up the speed goes from 20->0 and when he falls down again it goes from 0->20
+      this.frameY = 2;
+      this.maxFrame = 7;
+    } else {
+      this.x = 0;
+    }
+    //horisontal movement - character cant move outside canvas
+    if (this.x < -80) this.x = -80;
+    else if (this.x > this.gameWidth - this.width + 80)
+      this.x = this.gameWidth - this.width + 80;
+    //vertical movement - velocity when jumping and telling which sprite to use.
+    this.y += this.vy;
+    if (!this.onGround()) {
+      this.vy += this.weight;
+      this.maxFrame = 6;
+      //this.frameY = 0;
+    } else {
+      this.vy = 0;
+      this.maxFrame = 9;
+      //this.frameY = 2;
+    }
+    if (this.y > this.gameHeight - this.imgHeight)
+      this.y = this.gameHeight - this.imgHeight;
+  }
+  onGround() {
+    return this.y >= this.gameHeight - this.imgHeight;
   }
 }
 
@@ -106,8 +140,13 @@ function controlScreen() {
   clear();
 }
 
-function draw() {
-  gameScreen();
+function animate() {
   player.draw();
   player.update();
+  requestAnimationFrame(animate);
+}
+
+function draw() {
+  gameScreen();
+  animate();
 }
