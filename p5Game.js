8bda6edd90d3fe583e0a42e8c855c090d2f3f928
olne23pf <nellieolssonwennberg@gmail.com>
2024-05-08 22:54:18 +0200

    upd + added screens

--- p5Game.js
+++ p5Game.js
@@ -1,17 +1,33 @@
-let canvasWidth = 700;
-let canvasHeight = 500;
+let canvasWidth = 600;
+let canvasHeight = 600;
+let gameOver = false;
 let backgroundimg;
 let startimg;
 let img;
 let myFont;
 let button;
 let button2;
+let button3;
 
 let gameHeight;
 let gameWidth;
 let player;
+
 let log;
+let dx;
+let dy;
+let distance;
+let playerRadius;
+let logRadius;
+let totalRadius;
 let logs = [];
+
+let meatball;
+let coins = [];
+let score = 0;
+let firstPlace = 1000;
+let secondPlace = 20;
+let thirdPlace = 300;
 
 function preload() {
   // myFont = loadFont("LilitaOne-Regular.ttf");
@@ -22,6 +38,7 @@ function preload() {
   img = loadImage("images/sprites2.png");
   //for control screen
   controlimg = loadImage("images/controlscreen_image.png");
+  resultimg = loadImage("images/resultscreen_image.png");
 }
 
 function setup() {
@@ -30,21 +47,26 @@ function setup() {
   startScreen();
   textFont("Times New Roman");
   player = new Player(canvasWidth, canvasHeight); //creates player
-  log = new Log(canvasWidth, canvasHeight); //creates log
-
+  // Create logs and add them to the logs array
+  for (let i = 0; i < 5; i++) {
+    logs.push(new Log(canvasWidth, canvasHeight));
+  }
+  meatball = new Meatball(canvasWidth, canvasHeight);
   //Play Button
   button = createButton("Start Game");
-  button.position(220, 410);
+  button.position(180, 435);
   button.size(110, 34);
   button.mousePressed(gameScreen);
   button2 = createButton("Controls");
-  button2.position(400, 410);
+  button2.position(350, 435);
   button2.size(110, 34);
   button2.mousePressed(controlScreen);
 }
 
 function startScreen() {
   background(startimg);
+  screen = "start screen";
+  //button3.remove();
   /* textSize(65);
   textAlign(CENTER);
   textStyle(BOLD);
@@ -53,11 +75,42 @@ function startScreen() {
   text("meatballs", width / 2, height / 2);*/
 }
 
+function controlScreen() {
+  clear();
+  screen = "controls screen";
+  background(controlimg);
+
+  push();
+  noStroke();
+  fill(210, 210, 250);
+  rect(26, 30, 80, 30);
+  fill("black");
+  textSize(18);
+  text("back", 50, 50);
+  pop();
+
+  button.remove();
+  button2.remove();
+}
+
 function gameScreen() {
   clear();
+  screen = "game screen";
   background(backgroundimg);
   button.remove();
   button2.remove();
+  //button3.remove();
+}
+function reloadGameScreen() {
+  clear();
+  gameScreen();
+  for (let i = 0; i < logs.length; i++) {
+    logs[i].draw();
+    logs[i].update();
+    collision(player, logs[i]); // Check collision with each log
+  }
+  player.update();
+  player.draw();
 }
 
 class Player {
@@ -83,11 +136,10 @@ class Player {
 
   draw() {
     //create sprite image by https://p5js.org/reference/#/p5/image
-    //push();
     image(
       img,
       this.x + 250,
-      this.y - 95,
+      this.y - 75,
       this.imgWidth,
       this.imgHeight,
       this.frameX * this.imgWidth,
@@ -95,7 +147,6 @@ class Player {
       this.imgWidth,
       this.imgHeight
     ); //draws the player
-    //pop();
   }
 
   update() {
@@ -124,7 +175,7 @@ class Player {
       this.previousDirection = "left";
     } else if (keyIsPressed && keyCode == UP_ARROW) {
       this.maxFrame = 6;
-      this.y = this.y - 15; //highness of the jump
+      this.y = this.y - 25; //highness of the jump
       // got help with writing previous direction detection code from https://chatgpt.com/c/5b563428-b8d0-44be-b4b3-cc8fcd2e7562
       if (this.previousDirection === "right") {
         //this.vy -= 2; //velocity on 20, meaning that when jumping up the speed goes from 20->0 and when he falls down again it goes from 0->20
@@ -157,8 +208,8 @@ class Player {
     }
 
     //CANT RUN OR JUMP OUTSIDE CANVAS
-    if (this.x > 310) {
-      this.x = 310;
+    if (this.x > 210) {
+      this.x = 210;
     }
     if (this.x < -310) {
       this.x = -310;
@@ -178,8 +229,8 @@ class Log {
     this.gameHeight = gameHeight;
     this.imgWidth = 200;
     this.imgHeight = 170;
-    this.x = -100;
-    this.y = 330;
+    this.x = -200;
+    this.y = 450;
     this.frameX = 0;
     this.frameY = 4;
     this.maxFrame = 4;
@@ -187,19 +238,13 @@ class Log {
     this.frameTimer = 4;
     this.frameInterval = 1000 / this.fps;
     this.speed = 9;
-    this.logTimer = random(100, 200);
-    this.logInterval = random(100, 200);
+    this.logTimer = 0;
+    this.logInterval = 1000;
+    this.randomLogInterval = Math.random() * 1000 + 500;
     //this.markedForDeletion = false;
   }
 
   draw(context) {
-    /* context.strokeStyle = "black";
-    context.strokeRect(
-      this.x + 90,
-      this.y + 80,
-      this.width - 300,
-      this.height - 200
-    ); */ //box around logs to easily detect collision
     image(
       img,
       this.x,
@@ -222,41 +267,130 @@ class Log {
       this.frameTimer += deltaTime;
     }
     this.x += this.speed;
+
     //create more logs
-    this.logTimer += deltaTime; //creates more logs
-    if (this.logTimer > this.frameInterval) {
-      logs.push(new Log(canvasWidth, canvasHeight));
-      this.logTimer = 0;
-      this.logInterval = random(100, 200);
+    if (this.x > canvasWidth) {
+      this.x = -200;
     }
-    //logs = logs.filter((log) => !log.markedForDeletion); //all logs are tested and checked, if the markedfordeletion property is false. Only the amount of logs in picture are counted in console.log
   }
 }
 
-function controlScreen() {
-  clear();
-  background(controlimg);
+class Meatball {}
 
+function collision(player, log) {
+  let pRectX = player.x + 365;
+  let pRectY = player.y - 50;
+  let pRectWidth = player.imgWidth - 240;
+  let pRectHeight = player.imgHeight - 30;
+
+  let lRectX = log.x + 90;
+  let lRectY = log.y + 30;
+  let lRectWidth = log.imgWidth - 250;
+  let lRectHeight = log.imgHeight - 120;
+  push();
+  stroke(50);
+  fill("rgba(0,0,0,0)");
+  rect(pRectX, pRectY, pRectWidth, pRectHeight);
+  stroke(50);
+  fill("rgba(0,0,0,0)");
+  rect(lRectX, lRectY, lRectWidth, lRectHeight);
+  pop();
+
+  //got help with what to write in this if-statement from: https://chatgpt.com/c/018a26bb-9246-423e-aac0-c394643ad1db
+
+  if (
+    pRectX < lRectX + lRectWidth &&
+    pRectX + pRectWidth < lRectX &&
+    pRectY < lRectY + lRectHeight &&
+    pRectY + pRectHeight > lRectY
+  ) {
+    gameOver = true;
+    resultScreen();
+  } else {
+    gameOver = false;
+  }
+}
+
+function resultScreen() {
+  clear();
+  screen = "result screen";
+  background(resultimg);
+  push();
+  textSize(18);
+  textStyle(BOLD);
+  textAlign(CENTER);
+  text("SCORE: " + score, canvasWidth / 2, 255);
+  pop();
+  push();
+  textSize(18);
+  textStyle(BOLD);
+  textAlign(RIGHT);
+  text(firstPlace + " p", canvasWidth / 2 + 70, 345);
+  text(secondPlace + " p", canvasWidth / 2 + 70, 400);
+  text(thirdPlace + " p", canvasWidth / 2 + 70, 452);
+  pop();
+  push();
+  textSize(18);
+  textAlign(CENTER);
+  text("Play Again", canvasWidth / 2, 540);
+  pop();
   button.remove();
   button2.remove();
 }
 
+function mousePressed() {
+  if (
+    screen === "controls screen" &&
+    mouseX > 26 &&
+    mouseX < 106 &&
+    mouseY > 30 &&
+    mouseY < 60
+  ) {
+    screen = "start screen"; // Go back to the start screen
+    setup();
+  } else if (
+    screen === "result screen" &&
+    mouseX > 250 &&
+    mouseX < 350 &&
+    mouseY > 520 &&
+    mouseY < 545
+  ) {
+    screen = "reload game screen"; // Go back to the start screen
+  }
+}
+
 function animate() {
-  player.draw();
-  player.update();
-  log.draw();
-  log.update();
+  reloadGameScreen();
 }
 
 function draw() {
-  //startScreen();
-  gameScreen();
-  animate(0);
-  //controlScreen();
-  for (let i = 0; i < logs.length; i++) {
-    logs[i].draw();
+  // gameScreen();
+  //  player.draw();
 
-    //logs[i].update();
+  // resultScreen();
+  /*if (collision(player, log)) {
+    gameOver = true;
+  }*/
+  if (screen === "start screen") {
+    startScreen();
+  } else if (screen === "game screen") {
+    gameScreen();
+    //animate(0);
+    if (!gameOver) {
+      animate(0);
+    }
+  } else if (screen === "controls screen") {
+    controlScreen();
+
+    /*if (20 < mouseX < 100 && 20 < mouseY < 100 && mouseIsPressed) {
+      screen = "game screen";
+    }*/
+  } else if (screen === "result screen") {
+    resultScreen();
+    /*if (220 < mouseX < 350 && 520 < mouseY < 545 && mouseIsPressed) {
+      screen = "start screen";
+    }*/
+  } else if (screen === "reload game screen") {
+    reloadGameScreen();
   }
 }
-console.log(logs);
